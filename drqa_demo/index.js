97019d11db3fb7e3797a7b6a0e4332d0f20b888f
gfgkmn <gfgkmn@gmail.com>
2017-08-28 20:10:41 +0800

    add translate use xiaoyi to demo

--- drqa_demo/index.js
+++ drqa_demo/index.js
@@ -1,6 +1,6 @@
 (function() {
 
-    var PNUM = 2 
+    var PNUM = 21 
     var paragraph_id = 0;
     var titles;
     var contextss;
@@ -15,6 +15,33 @@
         var qadiv = document.getElementById("qa");
         qadiv.innerHTML = "";
         displayQuestion();
+    }
+
+    function createXMLHttpObject() {
+        var XHRFactory = [
+            function () {
+                return new XMLHttpRequest();
+            },
+            function () {
+                return new ActiveXObject('Msxml2.XMLHTTP');
+            },
+            function () {
+                return new ActiveXObject('Msxml3.XMLHTTP');
+            },
+            function () {
+                return new ActiveXObject('Microsoft.XMLHTTP');
+            }
+        ];
+        var xhr = false;
+        for (var i = 0; i < XHRFactory.length; i++) {
+            try {
+                xhr = XHRFactory[i]();
+            } catch (e) {
+                continue;
+            }
+            break;
+        }
+        return xhr;
     }
 
     function load() {
@@ -62,7 +89,7 @@
         // input for Qustion
         var input = document.createElement("input");
         input.type = "text";
-        input.name = "question"
+        input.name = "question";
         input.classList.add("form-control");
         input.id = "question";
         // button to submit
@@ -81,6 +108,14 @@
         clear.classList.add("btn-default");
         clear.innerHTML = "clear";
         clear.id = "clear";
+        // button to translate 
+        var translate = document.createElement("button");
+        translate.type = "button";
+        translate.classList.add("btn");
+        translate.classList.add("btn-sm");
+        translate.classList.add("btn-default");
+        translate.innerHTML = "translate";
+        translate.id = "translate";
         // loading
         var loading = document.createElement("div");
         loading.id = "loading";
@@ -96,12 +131,14 @@
         form.appendChild(input);
         form.appendChild(button);
         form.appendChild(clear);
+        form.appendChild(translate);
         form.appendChild(loading);
         var qadiv = document.getElementById("qa");
         qadiv.append(form);
 
         button.onclick = loadAnswer;
         clear.onclick = clearField;
+        translate.onclick = translateTriple;
     }
 
     function displayAnswer(answer) {
@@ -115,6 +152,7 @@
         input.style = "resize:none";
         input.readOnly = true;
         input.classList.add("form-control");
+        input.name = "answer";
         input.innerHTML = answer;
         div.appendChild(label);
         label.appendChild(span);
@@ -133,10 +171,63 @@
         sendAjax("/submit", data, handleAnswer);
     }
 
+
+    function translateTriple() {
+        translateContext($("#paragraph").val(), 'paragraph_t');
+        translateContext($("input[name='question']")[0].value, 'question_t');
+        translateContext($("textarea[name='answer']")[0].value, 'answer_t');
+
+    }
+
+    function translateContext(source_text, target_id) {
+        // var source_text = $("#paragraph").val();
+        // var target_id = 'paragraph_t';
+
+        var xhr = createXMLHttpObject();
+        xhr.open('POST', 'https://api.interpreter.caiyunai.com/v1/translator?model=onmt:v2', true);
+        xhr.setRequestHeader('content-type', 'application/json');
+        xhr.setRequestHeader('X-Authorization', 'token j1np9nb4h8jad0mi2odk');
+
+
+        xhr.onreadystatechange = function (event) {
+            if (xhr.readyState == 4) {
+                var res = xhr.status == 200 ? JSON.parse(xhr.responseText) : null;
+
+                if (res && res.rc == 0) {
+                    var target = res.target;
+                    var curr = document.getElementById("translate_p");
+                    curr.removeChild(document.getElementById(target_id));
+                    // curr.removeChild(document.getElementById("question_t"));
+                    // curr.removeChild(document.getElementById("answer_t"));
+                    var paragraph_t = document.createElement("p");
+                    paragraph_t.innerHTML = target;
+                    paragraph_t.id = target_id;
+                    curr.append(paragraph_t);
+                    console.log(target);
+                }
+                else {
+                    console.error(res);
+                }
+            }
+        };
+
+        xhr.send(JSON.stringify({
+            "source": source_text,
+            // question: $("input[name='question']")[0].val(),
+            // answer: $("input[name='answer']")[0].val(),
+            "trans_type": "en2zh",
+            "request_id": "a11111",
+            "replaced": true,
+            "cached": true
+        }));
+    }
+
+
     function handleAnswer(answer) {
         var curr = document.getElementById("current");
         curr.removeChild(document.getElementById("submit"));
         curr.removeChild(document.getElementById("clear"));
+        curr.removeChild(document.getElementById("translate"));
         curr.removeChild(document.getElementById("loading"));
         displayAnswer(answer);
         var q = document.getElementById("question");
@@ -150,8 +241,17 @@
         clear.classList.add("btn-default");
         clear.innerHTML = "new question!";
         clear.id = "clear";
-        clear.onclick = clearField;
+        var translate = document.createElement("button");
+        translate.type = "button";
+        translate.classList.add("btn");
+        translate.classList.add("btn-sm");
+        translate.classList.add("btn-default");
+        translate.innerHTML = "translate";
+        translate.id = "translate";
         curr.appendChild(clear);
+        curr.appendChild(translate);
+        clear.onclick = clearField;
+        translate.onclick = translateTriple;
     }
 
     function loadParagraph() {
